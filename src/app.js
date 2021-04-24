e3f9b816c83d7e36bd6f77a1d21f32d52269c00e
amirkh8006 <amirhossinkh7979@gmail.com>
2021-04-24 10:07:50 +0430

    start app

---
+++ src/app.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express()
+
+// Runnig Server
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server Running On Port ${port}`))
