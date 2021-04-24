c4825b85e9aede03492a4898f3bf265678b2e7a4
amirkh8006 <amirhossinkh7979@gmail.com>
2021-04-24 15:37:18 +0430

    add middleware

---
+++ src/middleware/auth.js
@@ -0,0 +1,13 @@
+async function checkIp(req, res, next) {
+    if (req.ip === "192.168.1.1") {
+        next();
+    } else {
+        return res.status(403).json({
+            "error": "unauthorized"
+        });
+    }
+}
+
+module.exports = {
+    checkIp
+};
