addfc03751f500351dabceb90d96df0a7847e838
amirkh8006 <amirhossinkh7979@gmail.com>
2021-04-24 15:35:18 +0430

    add route to app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
-const app = express()
+const app = express();
 
 // Runnig Server
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Server Running On Port ${port}`))
+
+module.exports = app
+const routes = require('./routes/detail')
