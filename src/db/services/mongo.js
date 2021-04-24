4668c93786f2be388c92d0335384fc27354ce18a
amirkh8006 <amirhossinkh7979@gmail.com>
2021-04-24 15:36:22 +0430

    add mongo functions for add and get

---
+++ src/db/services/mongo.js
@@ -0,0 +1,33 @@
+const Detail = require('../model/Detail');
+
+async function addDetail(data) {
+    return new Promise((resolve, reject) => {
+        const detail = new Detail(data)
+
+        detail.save()
+            .then((data) => {
+                resolve(data)
+            })
+            .catch((err) => {
+                reject(err)
+            });
+    })
+
+}
+
+async function getDetail() {
+    return new Promise((resolve, reject) => {
+        Detail.find()
+            .then((data) => {
+                resolve(data)
+            })
+            .catch((err) => {
+                reject(err)
+            });
+    })
+}
+
+module.exports = {
+    addDetail,
+    getDetail
+};
