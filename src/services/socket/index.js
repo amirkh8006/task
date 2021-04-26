a9797549d3cae6a14bc81ae9d329a3ec30666786
amirkh8006 <amirhossinkh7979@gmail.com>
2021-04-26 13:48:21 +0430

    move socket to services

---
+++ src/services/socket/index.js
@@ -0,0 +1,28 @@
+const getData = require("../price/index")
+let express = require('express')
+let app = express();
+let cors = require('cors')
+
+app.use(cors())
+
+let http = require('http');
+let server = http.Server(app);
+
+let socketIO = require('socket.io');
+let io = socketIO(server , {
+    cors: {
+      origin: "http://localhost:4200",
+      credentials: true
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log('user connected');
+    getData().then((dt)=>{
+        socket.emit("test" , dt);
+    })
+    .catch((error) => {
+        // handle error
+        console.log(error);
+    })
+});
