909eb4db9a431e729ffee84e49bde2d41e2defee
amirkh8006 <amirhossinkh7979@gmail.com>
2021-04-26 13:47:02 +0430

    use functions insted of async await

--- src/routes/detail.js
+++ src/routes/detail.js
@@ -1,64 +1,59 @@
-const {app} = require('../app')
-const axios = require('axios')
-const job = require('../services/server')
+const express = require('express');
+const router = express.Router();
+const job = require('../services/cronJobs/index')
 const mongoService = require('../db/services/mongo')
 const redisService = require('../db/services/redis')
+const {
+    successHandler
+} = require('../components/handler/successHandler')
+const {
+    errorHandler
+} = require('../components/handler/errorHandler')
+const getData = require("../services/price/index")
 
 
 // JOB ROUTES
 
-router.route('/startJob')
-    .get()
-    .post()
+router
+    .get('/startJob', async (req, res) => {
+        job.start()
+        // return res.send("Job Started...")
+        return successHandler(res, 200, "Job Started...")
+    })
+    .get('/stopJob', async (req, res) => {
+        job.stop()
+        return successHandler(res, 200, "Job Stoped...")
+    })
+    .get('/jobStatus', async (req, res) => {
+        const status = job.running
+        if (!status) {
+            return successHandler(res, 200, "Job Has Stopped...")
+        } else {
+            return successHandler(res, 200, "Job Is Running...")
+        }
+    })
+    .get('/getData', async (req, res) => {
+        getData().then((data) => {
+            return successHandler(res, 200, data)
+        }).catch(err => {
+            return errorHandler(res, 400, err)
+        })
+    })
+    .get('/getDataFromDB', async (req, res) => {
+        mongoService.getDetail().then((dt) => {
+                return successHandler(res, 200, dt)
+            })
+            .catch(err => {
+                return errorHandler(res, 400, err)
+            })
+    })
+    .get('/getDataFromRedis', async (req, res) => {
+        redisService.hget("test", "data").then((dt) => {
+                return successHandler(res, 200, dt)
+            })
+            .catch(err => {
+                return errorHandler(res, 400, err)
+            })
+    })
 
-app.get('/startJob', async (req, res) => {
-    job.start()
-    // return res.send("Job Started...")
-    return SuccessHandler(res, 200, result, message)
-})
-
-app.get('/stopJob', async (req, res) => {
-    job.stop()
-    res.send("Job Stoped...")
-})
-
-app.get('/jobStatus' , async (req , res)=>{
-    const status = job.running
-    if (!status) {
-        res.send("Job Has Stopped...")
-    }else{
-        res.send("Job Is Running...")
-    }
-})
-
-
-//Get Data
-app.get('/getData', async (req, res) => {
-    try {
-        const response = await axios.get('https://call14.tgju.org/ajax.json?2021042411-20210424112300-l2CJHyUf9eOGYaS0rTAt');
-        console.log();
-        res.send(response.data)
-    } catch (error) {
-        console.error(error);
-        return ErrorHandler()
-    }
-    // Services.price.getDate()
-    //     .then((result) => {
-    //     return SuccessHandler(res, 200, result, message)
-
-    //     })
-    //     .catch(() = > {
-    //     return ErrorHandler()
-
-    //     })
-})
-
-app.get('/getDataFromDB', async (req, res) => {
-    const dt = await mongoService.getDetail();
-    res.send(dt)
-})
-
-app.get('/getDataFromRedis', async (req, res) => {
-    const dt = await redisService.hget("test", "data");
-    res.send(JSON.parse(dt))
-})
+module.exports = router;
