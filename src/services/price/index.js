a95019f43838cb810f5ace023c5e6c4f31311aa5
amirkh8006 <amirhossinkh7979@gmail.com>
2021-04-26 13:48:07 +0430

    add getData function

--- src/services/price/index.js
+++ src/services/price/index.js
@@ -1,1 +1,17 @@
-// function getData
+const Promise = require('bluebird')
+const axios = require('axios');
+
+const getData = () => {
+    return new Promise((resolve, reject) => {
+        axios.get('https://call14.tgju.org/ajax.json?2021042411-20210424112300-l2CJHyUf9eOGYaS0rTAt')
+        .then((response) => {
+            // handle success
+            return resolve(response.data)
+        }).catch((error) => {
+            // handle error
+            return reject(error)
+        })
+    })
+}
+
+module.exports = getData
