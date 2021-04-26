9a9d43fa345806d6d3333e8fae001212f672da2f
amirkh8006 <amirhossinkh7979@gmail.com>
2021-04-26 13:47:44 +0430

    add cronJob service

--- src/services/cronJobs/index.js
+++ src/services/cronJobs/index.js
@@ -1,21 +1,49 @@
-// const { getDate, save } = require("../../db/driver/redis/connection")
-// // save
-// // redis
+const getData = require("../price")
+const mongoService = require('../../db/services/mongo')
+const redisService = require('../../db/services/redis')
+const CronJob = require('cron').CronJob;
 
 
-// const job = new CronJob('*/5 * * * * *', function () {
-//   functionJob()
-// }
+let redisData = []
+const functionJob = () => {
+    getData()
+        .then((data) => {
+            let dt = data.current.price_dollar_rl
+            mongoService.addDetail({
+                p: dt.p,
+                h: dt.h,
+                l: dt.l,
+                d: dt.d,
+                dp: dt.dp,
+                dt: dt.dt,
+                t: dt.t,
+                ts: dt.ts
+            })
+            return dt
+        })
+        .then((dt) => {
+            if (redisData.length >= 10) {
+                redisData.push({
+                    timeStamp: new Date(),
+                    data: dt
+                });
+                redisData.shift()
+                return redisService.hset("test", ["data", JSON.stringify(redisData)])
+            } else {
+                redisData.push({
+                    timeStamp: new Date(),
+                    data: dt
+                });
+                return redisService.hset("test", ["data", JSON.stringify(redisData)])
+            }
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+}
 
-// // const functionJob = () => {
-// //   getData()
-// //     .then(() => {
-// //       return save()
-// //     })
-// //     .then(() => {
-// //       return saveINRedis()
-// //     })
-// //     .catch((err) => {
-// //       console.log(err)
-// //     })
-// // }
+const job = new CronJob('*/5 * * * * *', () => {
+    functionJob();
+})
+
+module.exports = job
